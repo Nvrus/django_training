9202b9c3003bb01c507fa233dcb94a050f9af798
Nvrus <nvrus.arsenal@mail.ru>
2022-10-19 18:05:53 +0700

    fourth some commit 19.oct.2022 18:05 All components are working! Cheakpoint

--- crm/views.py
+++ crm/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
+from cms.models import CmsSlider
 
 # Create your views here.
 
 def first_page(request):
-    object_list = Order.objects.all()
-    form = OrderForm()
+    slider_list = CmsSlider.objects.all()
     return render(request, './index.html', {
-        'object_list': object_list,
-        'form':form
+        'slider_list': slider_list,
+
     })
 
 def thanks_page(request):
